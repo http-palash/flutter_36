228eb9d8a6f13998723896341a8c9a7bdd305193
Palash Bajpai <palashbajpai1@gmail.com>
2025-04-05 19:45:05 +0530

    Initial

---
+++ 05/images/lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Images'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Image.asset(
+        'assets/images/download.jpg',
+        fit: BoxFit.cover,
+        width: double.infinity,
+        height: double.infinity,
+      ),
+      // body: Image.asset('assets/images/.jpg', fit: BoxFit.cover),
+      // body: Image.asset('assets/images/.jpg', fit: BoxFit.cover),
+
+      // body: Image.network(
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ 06/columns_and_rows/android/app/src/main/kotlin/com/example/columns_and_rows/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.columns_and_rows
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
